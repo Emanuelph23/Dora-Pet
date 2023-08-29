dd93c216f01e4451a4e597feb469f02c42d42c6c
Emanuelph23 <emanuelph6@gmail.com>
2023-08-28 21:23:54 -0300

    Feat: Config middleware to upload image of the users

---
+++ backend/helpers/image-upload.js
@@ -0,0 +1,36 @@
+const multer = require('multer');
+const path = require('path');
+
+//Config path to save images
+const imageStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+
+        let folder = ""
+
+        if(req.baseUrl.includes('users')) {
+            folder = 'users';
+        } else if(req.baseUrl.includes('pets')) {
+            folder = 'pets';
+        }
+
+        cb(null, `public/images/${folder}`);
+
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter: function(req, file, cb) {
+
+        if(!file.originalname.match(/\.(jpg|png)$/)) {
+            return cb(new Error('Por favor, envie uma imagem válida!'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+module.exports = { imageUpload }
