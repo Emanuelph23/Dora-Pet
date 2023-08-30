141a82143626a3d8963774865564be6388a9bb91
Emanuelph23 <emanuelph6@gmail.com>
2023-08-30 15:47:52 -0300

    Feat: Config routes of the pets

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,10 @@ app.use(express.static('public'));
 
 //Config routes
 const UserRoutes = require('./routes/UserRoutes');
+const PetRoutes = require('./routes/PetsRoutes');
 
 app.use('/users', UserRoutes);
+app.use('/pets', PetRoutes);
 
 app.listen(5000, () => {
     console.log('Server running on port 5000')
---
+++ backend/routes/PetsRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+//Import Pets Controller
+const PetsController = require('../controllers/PetsController');
+const {imageUpload} = require('../helpers/image-upload');
+
+//Middlewares
+const verifyToken = require('../helpers/verify-token');
+
+//Routes
+router.post('/create', verifyToken, imageUpload.array('imagespet') ,PetsController.createPet);
+router.get('/', PetsController.listPets);
+router.get('/mypets', verifyToken,PetsController.getAllUserPets);
+router.get('/myadoptions', verifyToken ,PetsController.getAllUserAdoptions);
+router.get('/:id', PetsController.getPetById);
+router.delete('/:id', verifyToken, PetsController.removePetById);
+router.patch('/edit/:id', verifyToken, imageUpload.array('imagespet'), PetsController.updatePet);
+router.patch('/schedule/:id', verifyToken, PetsController.scheduleAdoption);
+router.patch('/conclude/:id', verifyToken, PetsController.concludeScheduleAdoption);
+
+module.exports = router;
