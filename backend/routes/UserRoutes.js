77ba60050994e7f3d40ecb6a20503d0e20b32834
Emanuelph23 <emanuelph6@gmail.com>
2023-09-01 17:02:07 -0300

    Feat: Config router of user to add items in shoppinig cart

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -11,6 +11,7 @@ const { imageUpload } = require('../helpers/image-upload');
 //Routes
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
+router.patch('/shopping/:id', checkToken ,UserController.shoppingUser);
 router.get('/checkuser', UserController.checkUser);
 router.get('/:id', UserController.getUserById);
 router.patch('/edit/:id', checkToken, imageUpload.single("avatar"),UserController.editUser);
