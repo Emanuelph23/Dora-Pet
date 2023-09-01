c6a83adfb2b3aa81d1f2f93da926f266c4aad728
Emanuelph23 <emanuelph6@gmail.com>
2023-09-01 16:59:51 -0300

    Refactor: update key username

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const {Schema} = mongoose;
 
 const UserSchema = mongoose.model('User', new Schema(
     {
-        username: {type: String, required: true, unique: true},
+        username: {type: String, required: true},
         password: {type: String, required: true},
         email: {type: String, required: true},
         avatar: {type: String},
